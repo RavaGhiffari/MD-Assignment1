8630db7c0a3a7eaca9807fa0385047faddbe5725
Rava <rava.ghiffari@gmail.com>
2025-03-23 07:11:06 +0000

    Progress 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,21 @@ st.title('🎈 App Name')
 
 import pandas as pd
 
-# Create a sample DataFrame
 
 df = pd.read_csv('ObesityDataSet_raw_and_data_sinthetic.csv')
+with st.expander("Data Overview"):
+    # Display the head of the DataFrame inside the expander
+    st.write('#### This is the raw data')
+    st.dataframe(df)
 
-# Display the DataFrame
-st.dataframe(df)
+column_name = st.selectbox(
+    "Select a column to display unique values",  # Label for the dropdown
+    df.select_dtypes(include=['object']).columns  # List of options (columns in the DataFrame)
+)
 
-st.markdown(df)
+# Display unique values from the selected column
+if column_name:
+    st.write(f"### Unique values in column '{column_name}':")
+    st.write(df[column_name].unique())
 
-# Display a static table
-st.table(df)
 
-st.write('Hello world!')
